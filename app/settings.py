2782bda7be5f41899e55216f83792b69eab40b32
Ryzen <sosnovscky.nikita@yandex.ru>
2022-02-13 21:03:24 +0300

    Updates

--- app/settings.py
+++ app/settings.py
@@ -99,6 +99,16 @@ REST_FRAMEWORK = {
     ]
 }
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+}
+
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
